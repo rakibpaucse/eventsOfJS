22143a92ee1f12991b938cbbcf11a160c7ee1d31
Rakib <rakibpaucse@gmail.com>
2021-04-14 21:58:03 +0600

    updated

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var form = document.querySelector('.form')
 var extras = document.querySelector('.extras')
 var wrapper = document.querySelector('.wrapper')
 
+var wrapper = document.querySelector('.wrapper')
+
 
 
 
@@ -72,8 +74,66 @@ selectedMode.addEventListener('copy', (event) => {
 
 
 var userName = document.querySelector('.userName')
+var password = document.querySelector('.password')
+
 userName.addEventListener('paste', (event) => {
     let paste = (event.clipboardData || window.clipboardData).getData('text');
     paste = paste.toUpperCase();
     alert(`You pasted:  ${paste}`)
 });
+
+
+// New 
+
+userName.addEventListener('focus' , (e) => {
+    e.target.style.background = 'white';
+    e.target.style.width = '300px';
+    e.target.style.color = '#53e3a6'; 
+
+})
+
+userName.addEventListener('blur' , (e) => {
+    e.target.style.background = 'rgba(255, 255, 255, 0.2)';
+    e.target.style.width = '250px';
+    e.target.style.color = 'white';
+})
+
+
+password.addEventListener('keydown', (e) => {
+   let code = e.keyCode || e.which;
+    console.log(String.fromCharCode(code));  
+});
+
+password.addEventListener('keyup', (e) => {
+    let code = e.keyCode || e.which;
+     console.log(String.fromCharCode(code));  
+});
+
+
+
+const zoom = (event) => {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+  
+    scale = Math.min(Math.max(.125, scale), 4);
+    makeLargeHolder.style.transform = `scale(${scale})`;
+}
+let scale = 1;
+makeLargeHolder.onwheel = zoom
+
+
+
+makeLargeHolder.addEventListener('mousemove', e => {
+console.log(e.offsetX, e.offsetY);
+})
+
+wrapper.addEventListener('mousedown', e => {
+    console.log('Mouse Down in ' + e.offsetX, e.offsetY);
+})
+
+wrapper.addEventListener('mouseup', e => {
+    console.log('Mouse Up in ' + e.offsetX, e.offsetY);
+})
+
+
+
